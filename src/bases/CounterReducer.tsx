bc4340d0ed363190dab39f78747bbadc151cdf2a
Gerguevara <gerardoguevara64@gmail.com>
2022-07-04 11:49:24 -0600

    comments for reducer 1 sinlgle file

--- src/bases/CounterReducer.tsx
+++ src/bases/CounterReducer.tsx
@@ -1,79 +1,78 @@
-import { useReducer } from 'react'
+import { useReducer } from 'react';
 
+// interfces del estado inicial
 interface CounterState {
-    counter : number;
-    previous: number;
-    changes : number;
+  counter: number;
+  previous: number;
+  changes: number;
 }
 
-
+//estado inicial valor minimo con el que nuestro state de reducer empieza
 const INITIAL_STATE: CounterState = {
-    counter: 0,
-    previous: 0,
-    changes: 0
-}
+  counter: 0,
+  previous: 0,
+  changes: 0,
+};
 
-type CounterAction = 
-    | { type: 'increaseBy', payload: { value: number; } }
-    | { type: 'reset' };
+//typos de acciones soportadas por el reducer, ts va indicar si no es correcta
+type CounterAction =
+  | { type: 'increaseBy'; payload: { value: number } }
+  | { type: 'reset' };
 
-const counterReducer = ( state:CounterState, action:CounterAction ): CounterState => {
+//aciones del reducer, el cual siempre recibe un sate y el action
+const counterReducer = (
+  state: CounterState,
+  action: CounterAction
+): CounterState => {
+  // desestructuracion dde state anterior
+  const { counter, changes } = state;
 
-    const { counter, changes } = state;
+  switch (action.type) {
+    case 'reset':
+      //no necesita el payload pq los valores que va seteat ya son conocidos
+      return {
+        changes: 0,
+        counter: 0,
+        previous: 0,
+      };
 
-    switch ( action.type ) {
-        case 'reset':
-            return {
-                changes: 0, 
-                counter: 0,
-                previous: 0,
-            }      
-            
-        case 'increaseBy':
-            return {
-                changes: changes + 1,
-                counter: counter + action.payload.value,
-                previous: counter
-            }
-    
-        default:
-            return state;
-    }
-}
+    case 'increaseBy':
+      //si necesita el payload por ese asiganara el nuevo state
+      return {
+        changes: changes + 1,
+        counter: counter + action.payload.value,
+        previous: counter, // hace referencia al objeto desestructurado, por eso no sobreescribe el de ln 42, usa 28
+      };
 
+    default:
+      return state;
+  }
+};
 
-
+//la logica de nuestro componente
 export const CounterReducerComponent = () => {
+  const [counterState, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
-    const [ counterState, dispatch] = useReducer( counterReducer, INITIAL_STATE );
+  const handleReset = () => {
+    dispatch({ type: 'reset' });
+  };
 
-    const handleReset = () => {
-        dispatch({ type: 'reset' })
-    }
+  const increaseBy = (value: number) => {
+    dispatch({ type: 'increaseBy', payload: { value } });
+  };
 
-    const increaseBy = ( value: number ) => {
-        dispatch({ type: 'increaseBy', payload: { value } });
-    }
+  return (
+    <>
+      <h1>Counter Reducer</h1>
+      <pre>{JSON.stringify(counterState, null, 2)}</pre>
 
-    return (
-        <>
-            <h1>Counter Reducer</h1>   
-            <pre>
-                { JSON.stringify( counterState, null, 2 ) }
-            </pre>
+      <button onClick={() => increaseBy(1)}>+1</button>
+      <button onClick={() => increaseBy(5)}>+5</button>
+      <button onClick={() => increaseBy(10)}>+10</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
+};
 
-            <button onClick={ () => increaseBy(1) }>
-                +1
-            </button>
-            <button onClick={ () => increaseBy(5) }>
-                +5
-            </button>
-            <button onClick={ () => increaseBy(10) }>
-                +10
-            </button>
-            <button onClick={ handleReset }>
-                Reset
-            </button>
-        </>
-    )
-}
+// nota se recomienda usar un use reducer en lugar de un use state cuando el objeto de la informacioon que se comparte
+// es delicaod y consumido de varias formas diferent4es en varios lados
